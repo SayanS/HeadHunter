fdc63cbcb20f360978feaced7bf4d7d5f0a08648
s.garmaev <garmsayan@gmail.com>
2018-07-06 18:36:58 +0300

    Spring version

--- src/test/java/ru/hh/tests/GlobalSearchFunctionality.java
+++ src/test/java/ru/hh/tests/GlobalSearchFunctionality.java
@@ -1,15 +1,13 @@
 package ru.hh.tests;
 
-import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.hh.api.EndPoints;
 import ru.hh.api.RestAssuredConfiguration;
 import ru.hh.pages.HomePage;
 import ru.hh.pages.SearchResultsPage;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static io.restassured.RestAssured.given;
@@ -26,19 +24,22 @@ public class GlobalSearchFunctionality extends BaseTest {
 
         RequestSpecification requestSpecification = new RestAssuredConfiguration().getRequestSpecification();
 
-        searchResultsPage.getVacancyIdListOfActivePage()
-                .forEach(id -> {
-//            requestSpecification.queryParam("employeeId", 200).log().all();
-                    String str;
-                    Pattern p = Pattern.compile(">(.*?)<");
-
-
-                    Response response = given().spec(requestSpecification).get(EndPoints.GET_VACANCIES + "/" + id);
-                    str=response.getBody().jsonPath().get("description").toString();
-                    Matcher m = p.matcher(str);
-
-                    Arrays.stream(searchText.split(" ")).forEach(text -> response.then().body("description", org.hamcrest.Matchers.containsString(text)));
-                });
+        searchResultsPage.getVacancyIdListOfActivePage().forEach(id -> {
+            String description;
+            Boolean flag=false;
+            Pattern p = Pattern.compile("(<)(.*?)(>)");
+            description = String.join("", p.split(given().spec(requestSpecification)
+                    .get(EndPoints.GET_VACANCIES + "/" + id)
+                    .getBody()
+                    .jsonPath().get("description").toString()));
+            String expectedTexts[] = searchText.split(" ");
+            for (int i = 0; i <= expectedTexts.length - 1; i++) {
+               if(description.contains(expectedTexts[i])){
+                   flag=true;
+               }
+            }
+            Assert.assertTrue(flag, description +"\n of vacancy id="+id +"\n isn't contains any word from\n"+searchText);
+        });
     }
 
 }
